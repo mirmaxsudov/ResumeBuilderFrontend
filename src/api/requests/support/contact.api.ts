1acfff14a54f240328dfc1cb406a9d4a7e3db04f
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-09-14 12:20:19 +0500

    Abdurahmon: added apis for support contact.

--- src/api/requests/support/contact.api.ts
+++ src/api/requests/support/contact.api.ts
@@ -1,12 +1,39 @@
 import $api from "@/api/request";
-import ApiResponse from "@/types/ApiResponse";
-import { ContactRequestType } from "@/types/support/contactType";
+import { ContactSubjectEnum } from "@/enums/ContactSubjectEnum";
+import ApiResponse, { PageApiRespone } from "@/types/ApiResponse";
+import { ContactMessageResponse, ContactRequestType, ContactResponseType } from "@/types/support/contactType";
 
 const CONTACT_BASE_URL: string = "/api/v1/support/contact";
 
-const newContact = async (request: ContactRequestType): Promise<ApiResponse<string>> => {
-    const response = await $api.post<ApiResponse<string>>(CONTACT_BASE_URL + "/new", request);
-    return response.data;
+const SupportContactApis = {
+    newContact: async (request: ContactRequestType): Promise<ApiResponse<string>> => {
+        const response = await $api.post<ApiResponse<string>>(CONTACT_BASE_URL + "/new", request);
+        return response.data;
+    },
+    answerContact: async (id: number, answer: string): Promise<ApiResponse<string>> => {
+        const response = await $api.post(CONTACT_BASE_URL + "/" + id + "/answer", undefined, {
+            params: {
+                answer
+            }
+        });
+        return response.data;
+    },
+    getAll: async (page: number = 0, size: number = 10, query: string = "", search: string | ContactSubjectEnum = "ALL",): Promise<PageApiRespone<ContactResponseType[]>> => {
+        const response = await $api.get<PageApiRespone<ContactResponseType[]>>(CONTACT_BASE_URL + "/get", {
+            params: {
+                page, size, query, search
+            }
+        });
+        return response.data;
+    },
+    getMessagesByContactId: async (page: number = 0, size: number = 10, id: number): Promise<PageApiRespone<ContactMessageResponse[]>> => {
+        const response = await $api.get<PageApiRespone<ContactMessageResponse[]>>(`${CONTACT_BASE_URL}/${id}/messages`, {
+            params: {
+                page, size
+            }
+        });
+        return response.data;
+    }
 }
 
-export { newContact };
+export default SupportContactApis;
